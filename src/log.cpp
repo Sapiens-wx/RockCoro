cf389d4eae577484cff38a59c0be67256b7fef1b
Sapiens-wx <wx2194@nyu.edu>
2025-10-18 07:18:03 +0000

    different threads can log to the same file (./log)

--- src/log.cpp
+++ src/log.cpp
@@ -3,46 +3,59 @@
 
 #include <stdio.h>
 #include <atomic>
+#include <pthread.h>
 
 namespace rockcoro{
 
-std::atomic<int> _tid{0};
+static std::atomic<int> _tid{0};
 
-// helper function: get thread-local log file path
-static void get_thread_log_path(char* buf, size_t buf_size) {
-		int tmp=_tid.fetch_add(1);
-		snprintf(buf, buf_size, "./thread_logs/%d.log", tmp);
-}
-
-struct LoggerTL{
+struct Logger{
 	FILE* log_file;
-	LoggerTL(){
-		char path[256];
-		get_thread_log_path(path, sizeof(path)-1);
+	pthread_mutex_t mutex_log;
+	Logger(){
+		const char path[]="./log";
 		log_file = std::fopen(path, "w");
 		if (!log_file) {
 			printf("ERROR: Failed to open thread log file");
 		}
+		//init mutex
+		pthread_mutex_init(&mutex_log, nullptr);
 	}
-	~LoggerTL(){
+	~Logger(){
 		if(log_file)
 			fclose(log_file);
+		pthread_mutex_destroy(&mutex_log);
+		printf("logger destructor\n");
 	}
 };
 
-// 每个线程独有的 FILE*
-thread_local LoggerTL logger;
+struct LoggerTL{
+	int id;
+	LoggerTL(){
+		id=_tid.fetch_add(1);
+	}
+};
+
+static Logger logger;
+static thread_local LoggerTL logger_tl;
 
 void logf(const char* fmt, ...) {
 	char buffer[4096];
 
+	// print out the thread id first
+	int id_length=snprintf(buffer, sizeof(buffer), "[%d] ", logger_tl.id);
+
 	va_list args;
 	va_start(args, fmt);
-	int n = vsnprintf(buffer, sizeof(buffer), fmt, args);
+	int n = vsnprintf(buffer+id_length, sizeof(buffer)-id_length, fmt, args)+id_length;
 	va_end(args);
 
 	if (n > 0) {
+		pthread_mutex_lock(&logger.mutex_log);
 		fwrite(buffer, 1, n, logger.log_file);
+		printf(buffer);
+		fflush(logger.log_file);
+		pthread_mutex_unlock(&logger.mutex_log);
 	}
 }
 
